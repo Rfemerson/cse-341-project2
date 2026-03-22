49364f33e4f8539fa0ed3f1c7354c56f6ff902ca
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2026-03-21 21:46:36 -0300

    More fix documentation

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
     //#swagger.tags = ['Hello Emerson']
     res.send('Hello Emerson!')
@@ -8,5 +10,6 @@ router.get('/', (req, res) => {
 router.use('/vehicles', require('./vehicles'));
 
 router.use('/drivers', require('./drivers'));
+
 
 module.exports = router;
